f175322857cc51ec97c1d12e621bf7aa0b6cb5dc
charanwilliams <109322104+charanwilliams@users.noreply.github.com>
2024-02-02 23:45:44 -0600

    Fixed some styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,6 @@ import logoImg from '../assets/quiz-logo.svg'
 
 export default function Header() {
     return <header id="header">
-      <img src={logoImg} alt='logo'/> <h1>Sec+ Quiz</h1>
+      <img src={logoImg} alt='logo'/><h1>Sec+ Quiz</h1>
     </header>
 }
